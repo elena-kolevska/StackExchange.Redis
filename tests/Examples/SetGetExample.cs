9765afdd8429cf41c539b59f829a0c465ef872b0
Elena Kolevska <elena@kolevska.com>
2023-04-06 00:39:35 +0100

    First draft for getset the example. WIP.

---
+++ tests/Examples/SetGetExample.cs
@@ -0,0 +1,25 @@
+﻿// EXAMPLE: set_and_get
+// HIDE_START
+using System;
+using StackExchange.Redis;
+
+namespace Examples
+{
+    public class SetGetExample
+    {
+        public void SetGet()
+        {
+            // HIDE_END
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase db = redis.GetDatabase();
+
+            db.StringSet("bike:1", "Process 134");
+
+            string value = db.StringGet("bike:1");
+            Console.WriteLine(value);
+
+            // HIDE_START
+        }
+    }
+}
+// HIDE_END
---
+++ tests/Examples/SetGetTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Examples
+{
+    public class SetGetTests
+    {
+
+        [Fact]
+        public void SetGetTest()
+        {
+            try
+            {
+                private SetGetExample example = new SetGetExample();
+                example.SetGet();
+            }
+            catch (Exception e)
+            {
+                Assert.Null(e)
+            }
+        }
+
+    }
+}
